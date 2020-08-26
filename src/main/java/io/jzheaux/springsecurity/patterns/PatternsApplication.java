5e7cf257c14f8c33b398a4ddbfb2d9f30121c40d
Eleftheria Stein <eleftheria.kousathana@gmail.com>
2020-08-26 18:20:58 +0200

    Make application into a resource server

--- src/main/java/io/jzheaux/springsecurity/patterns/PatternsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/patterns/PatternsApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -26,6 +29,11 @@ public class PatternsApplication {
 		@GetMapping("/")
 		public String hello(@CurrentSecurityContext(expression="authentication.name") String name) {
 			return "Hello, " + name + "!";
+		}
+
+		@PostMapping("/location")
+		public String changeLocation(Authentication authentication, @RequestBody String location) {
+			return this.locationService.moveTo(location);
 		}
 	}
 
--- src/main/java/io/jzheaux/springsecurity/patterns/SecurityConfig.java
+++ src/main/java/io/jzheaux/springsecurity/patterns/SecurityConfig.java
@@ -2,27 +2,40 @@ package io.jzheaux.springsecurity.patterns;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails ria = User.withDefaultPasswordEncoder()
-                .username("ria")
-                .password("password")
-                .roles("USER")
-                .build();
-        UserDetails josh = User.withDefaultPasswordEncoder()
-                .username("josh")
-                .password("password")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(ria, josh);
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new CustomJwtGrantedAuthoritiesConverter());
+        return converter;
     }
 
+    public static class CustomJwtGrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        @Override
+        public Collection<GrantedAuthority> convert(Jwt jwt) {
+            Collection<GrantedAuthority> grantedAuthorities = jwtGrantedAuthoritiesConverter.convert(jwt);
+            Collection<GrantedAuthority> updatedGrantedAuthorities = new ArrayList<>(grantedAuthorities);
+            Set<String> grantedAuthorityStrings = AuthorityUtils.authorityListToSet(grantedAuthorities);
+            if (grantedAuthorityStrings.contains("SCOPE_aircraft.fly") || grantedAuthorityStrings.contains("SCOPE_boat.sail")){
+                updatedGrantedAuthorities.add(new SimpleGrantedAuthority("captain"));
+            }
+            return updatedGrantedAuthorities;
+        }
+    }
 }
