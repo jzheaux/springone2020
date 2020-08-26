9068996e1b5623e113622a1d8415242bbf6b7620
Josh Cummings <josh.cummings@gmail.com>
2020-08-26 14:47:45 -0600

    Method-based authorization

--- src/main/java/io/jzheaux/springsecurity/patterns/LocationService.java
+++ src/main/java/io/jzheaux/springsecurity/patterns/LocationService.java
@@ -1,21 +1,12 @@
 package io.jzheaux.springsecurity.patterns;
 
-import java.util.Set;
-
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LocationService {
+	@PreAuthorize("hasAuthority('captain')")
 	public String moveTo(String location) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		Set<String> authorities = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-		if (authorities.contains("captain")) {
-			return "You have moved to " + location;
-		}
-		throw new AccessDeniedException("You will not move today");
+		return "You have moved to " + location;
 	}
 }
--- src/main/java/io/jzheaux/springsecurity/patterns/PatternsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/patterns/PatternsApplication.java
@@ -2,16 +2,16 @@ package io.jzheaux.springsecurity.patterns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class PatternsApplication {
 
 	public static void main(String[] args) {
